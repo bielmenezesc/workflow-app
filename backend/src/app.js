e95909f49d4de98aa7fbf4d13a70c18591fa913e
bielmenezesc <gabriel.cabral@ccc.ufcg.edu.br>
2023-09-22 22:45:01 -0300

    Sending method edit workflow

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,12 +17,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.post('/workflows/create', async (req, res) => {
     const { name, initBox, endBox, conditionBox, actionBox } = req.body;
 
-    console.log(name);
-    console.log(initBox);
-    console.log(endBox);
-    console.log(conditionBox);
-    console.log(actionBox);
-
     try {
         const newWorkflow = new workflow({
             name,
@@ -41,22 +35,45 @@ app.post('/workflows/create', async (req, res) => {
 });
 
 // route to edit a workflow
-app.put('/workflows/edit/:id', async (req, res) => {
-    const { id } = req.params;
-    const updateData = req.body;
+app.put('/workflow/edit/:id', async (req, res) => {
+    const id = req.params;
+    const { name, initBox, endBox, conditionBox, actionBox } = req.body;
 
     try {
-        const updateWorkflow = await workflow.findByIdAndUpdate(id, updateData, {
-            new: true,
-        });
-        if (!updateWorkflow) {
+        const oldWorkflow = await workflow.findOne({"_id": new ObjectId(id)});
+
+        if (!oldWorkflow) {
             return res.status(404).json({error: 'Workflow not found.'});
         }
-        res.status(200).json(updateWorkflow);
+
+        oldWorkflow.name = name;
+        oldWorkflow.initBox = initBox;
+        oldWorkflow.endBox = endBox;
+        oldWorkflow.conditionBox = conditionBox;
+        oldWorkflow.actionBox = actionBox;
+
+        await oldWorkflow.save();
+
+        res.status(200).json(oldWorkflow);
     } catch (error) {
         res.status(500).json({ error: 'Error on editing the workflow.' });
     }
 });
+
+app.get('/workflow/edit/:id', async (req, res) => {
+    var id = req.params;
+
+    try {
+        const showWorkflow = await workflow.findOne({"_id": new ObjectId(id)});
+
+        if (!showWorkflow) {
+            return res.status(404).json({ error: 'Workflow not found.' });
+        }
+        res.status(200).json(showWorkflow);
+    } catch (error) {
+        res.status(500).json({ error: 'Error on showing the workflow.' });
+    }
+})
 
 // route to delete a workflow
 app.delete('/workflows/delete/:id', async (req, res) => {
@@ -77,12 +94,8 @@ app.delete('/workflows/delete/:id', async (req, res) => {
 app.get('/workflow/show/:id', async (req, res) => {
     var id = req.params;
 
-    console.log(id);
-
     try {
         const showWorkflow = await workflow.findOne({"_id": new ObjectId(id)});
-        
-        console.log(showWorkflow);
 
         if (!showWorkflow) {
             return res.status(404).json({ error: 'Workflow not found.' });
--- src/routes/editWorkflow.js
+++ src/routes/editWorkflow.js
@@ -1,13 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import "../App.css";
+import { useParams } from 'react-router-dom';
 
 function EditWorkflow() {
+  const [workflowData, setWorkflowData] = useState({});
+  const workflowId = useParams(); // Obtém o ID do workflow a ser editado
+
+  useEffect(() => {
+    // Carregue os detalhes do workflow pelo ID
+    Axios.get(`http://localhost:5000/workflow/edit/${workflowId.id}`)
+      .then((response) => {
+        setWorkflowData(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }, [workflowId]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setWorkflowData({ ...workflowData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Axios.put(`http://localhost:5000/workflow/edit/${workflowId.id}`, workflowData)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+
+  console.log(workflowData);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit Workflow
-        </p>
+        <h2>Editar Workflow</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={workflowData.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Beggining:</label>
+            <input
+              type="text"
+              name="initBox"
+              value={workflowData.initBox}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Condicion:</label>
+            <input
+              type="text"
+              name="conditionBox"
+              value={workflowData.conditionBox}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Action:</label>
+            <input
+              type="text"
+              name="actionBox"
+              value={workflowData.actionBox}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label>End:</label>
+            <input
+              type="text"
+              name="endBox"
+              value={workflowData.endBox}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit">Save Editions</button>
+        </form>
       </header>
     </div>
   );
